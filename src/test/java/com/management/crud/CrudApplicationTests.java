0e21e91a88ebfaa7966e3ed9e5a29461dd49994f
jubilee13 <bileesantos13@gmail.com>
2026-01-31 10:42:56 +0800

    [CHORE] Initialized springboot project

---
+++ src/test/java/com/management/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
